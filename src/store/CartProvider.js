1aabf88a68c8cfc1550c8d2b7bd050f96880d708
ali-ch88 <raza7861991@gmail.com>
2022-02-12 23:57:45 +0500

    new meal

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -8,7 +8,11 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   console.log(state);
-  
+  if (action.type === "ADD") {
+    const updatedItems = state.items.concat(action.item);
+
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
